6052b1aa84e1ed26d7214e03d20be2cea37234b1
Nadeen Shilbayeh <nshilbay@uwo.ca>
2024-01-13 16:19:56 -0500

    created coverletter generator page

---
+++ my-app/src/app/coverLetter/page.js
@@ -0,0 +1,11 @@
+'use client'
+
+import { useState } from 'react';
+
+export default function Home() {
+  const [resume, setResume] = useState(null);
+  const [jobDescription, setJobDescription] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [message, setMessage] = useState('');
+
+}
